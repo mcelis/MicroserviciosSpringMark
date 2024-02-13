ab820a97c1a734d8fe3095f676db015d3ba59c21
marco.celis <marco.celis@gmail.com>
2024-02-13 18:09:55 -0500

    BaseEntity adicionado con decoradores

--- accounts/src/main/java/com/ccti/accounts/entity/BaseEntity.java
+++ accounts/src/main/java/com/ccti/accounts/entity/BaseEntity.java
@@ -1,5 +1,6 @@
 package com.ccti.accounts.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 import lombok.Setter;
@@ -12,8 +13,12 @@ import java.time.LocalDateTime;
 @Getter
 @ToString
 public class BaseEntity {
+    @Column(name = "created_at",updatable = false)
     private LocalDateTime created_at;
+    @Column(name = "created_by",updatable = false)
     private String created_by;
+    @Column(name = "updated_at",insertable = false)
     private LocalDateTime updated_at;
+    @Column(name = "updated_by",insertable = false)
     private String updated_by;
 }
