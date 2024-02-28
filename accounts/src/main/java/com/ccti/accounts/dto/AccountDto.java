960b455d85bd67d22aa3f7f9087a5f08cacdcdc8
marco.celis <marco.celis@gmail.com>
2024-02-28 18:51:08 -0500

    Comentarios DTO y generacion de jar

--- accounts/src/main/java/com/ccti/accounts/dto/AccountDto.java
+++ accounts/src/main/java/com/ccti/accounts/dto/AccountDto.java
@@ -1,18 +1,34 @@
 package com.ccti.accounts.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
+@Schema(
+        name = "Accounts",
+        description = "Esquema que almacena datos de cuentas"
+)
 public class AccountDto {
 
+    @Schema(
+            description = "Numero de la cuenta en el sistema"
+    )
     @NotNull(message = "El numero de cuenta es requerido")
     //@Pattern(regexp = "(^$| [0-9](10}))", message = "El numero de cuenta debe contener 10 digitos")
     private Long accountNumber;
+
+    @Schema(
+            description = "Tipo de cuenta en el sistema 'Savings', 'Checking'"
+    )
     @NotEmpty(message = "El tipo de cuenta es requerido")
     private String accountType;
+
+    @Schema(
+            description = "Direccion sucursal de la cuenta en el sistema"
+    )
     @NotEmpty(message = "La direccion es requerida")
     private String branchAddress;
 }
